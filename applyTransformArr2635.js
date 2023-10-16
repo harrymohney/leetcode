5fabd255b894f13134869afeaee1da976dfa125e
harryhmohney <harrison.mohney@gmail.com>
2023-10-16 12:21:12 -0700

    Apply Transform Over Each Element in Array

---
+++ applyTransformArr2635.js
@@ -0,0 +1,10 @@
+function map(arr, fn) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+      const transformedValue = fn(arr[i], i);
+      result.push(transformedValue);
+  }
+
+  return result;
+}
